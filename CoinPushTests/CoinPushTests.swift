f2a9267aab6d79b5ae8568b14b209d57ecf19f6f
AndyQ <andy.qua@gmail.com>
2014-07-03 12:52:14 +0100

    Initial Commit

---
+++ CoinPushTests/CoinPushTests.swift
@@ -0,0 +1,35 @@
+//
+//  CoinPushTests.swift
+//  CoinPushTests
+//
+//  Created by Andy Qua on 03/07/2014.
+//  Copyright (c) 2014 Andy Qua. All rights reserved.
+//
+
+import XCTest
+
+class CoinPushTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
